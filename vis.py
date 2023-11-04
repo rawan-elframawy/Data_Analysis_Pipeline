3d23514859816ca71cf58787abf309eb357a69e8
MohamedAtta-AI <M.TarekSaad@nu.edu.eg>
2023-11-04 02:31:06 +0200

    visualization

--- vis.py
+++ vis.py
@@ -1,7 +1,23 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import sys
+import subprocess
 
+
+# Import data
+df = pd.read_csv('non-encoded.csv')
+
+categorical_column = 'Education'
+numerical_column = 'total_num_of_purchases'
+# Create a box plot to visualize the relationship
+plt.figure(figsize=(10, 6))
+sns.boxplot(x=categorical_column, y=numerical_column, data=df, palette='Set2')
+plt.title(f'Box Plot: {categorical_column} vs. {numerical_column}')
+plt.xlabel(categorical_column)
+plt.ylabel(numerical_column)
+plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+plt.grid(True)
 
 # Save the figure
-plt.savefig('plot.png', bbox_inches='tight')
-
-# Close the figure after saving to free up memory
-plt.close(fig)
+plt.savefig('vis.png', bbox_inches='tight')
